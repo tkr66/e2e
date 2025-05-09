75e7ece72a3fb18e87fbc49af4b23790e51ef84a
tkr66 <plumium@outlook.com>
2025-05-09 18:38:14 +0900

    add: test for cli.rs

--- src/cli.rs
+++ src/cli.rs
@@ -14,3 +14,17 @@ pub struct Args {
     #[arg(short, long, default_value = "e2e.yaml")]
     pub file: PathBuf,
 }
+
+#[cfg(test)]
+mod cli_tests {
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        let args = ["e2e", "-f", "example-e2e.yaml", "s1", "s2"];
+        let args: Args = Args::parse_from(args);
+
+        assert_eq!(Some(vec!["s1".to_string(), "s2".to_string()]), args.names);
+        assert_eq!(PathBuf::from("example-e2e.yaml"), args.file);
+    }
+}
