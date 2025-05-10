cad8551eb843d31c32b65032824efa9465110753
tkr66 <plumium@outlook.com>
2025-05-11 02:40:28 +0900

    update

--- example.sh
+++ example.sh
@@ -4,4 +4,4 @@ cargo build --release
 
 docker run -d -p 4444:4444 --rm --shm-size="2g" selenium/standalone-edge:4.32.0-20250505
 
-./target/release/e2e -f example-e2e.yaml
+./target/release/e2e -f example-e2e.yaml run
